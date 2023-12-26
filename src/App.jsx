405b144701254ad38c0a312606da713a02f17e91
“Matt” <mhallrp@gmail.com>
2023-12-26 16:27:04 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Quiz from './Pages/Quiz';
 import Login from './Pages/Login';
+import Unknown from './Pages/Unknown'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <Login />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Login /> }/>
+                <Route path= "/quiz" element={<Login />}/>
+                <Route path="*" element={ <Login /> }/>
+            </Routes>
+        </ BrowserRouter>
     );
 };
 

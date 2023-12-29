94d819c67182bcec5f7abecf9ba157c7bf869c92
“Matt” <mhallrp@gmail.com>
2023-12-29 20:27:33 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Styles from './styles.module.css';
 import Login from './Components/Login';
 import Quiz from './Components/Quiz';
 import useAuth from './Model/useAuth';
 import { useTriviaQuestions, useQuizCategories } from './Model/CustomHooks';
-import NavBar from './Components/NavBar'
+import NavBar from './Components/NavBar';
 
-export default function App () {
-
+export default function App() {
     const { sessionCheck } = useAuth();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [currentCategories, setCurrentCategories] = useState(null);
@@ -17,6 +16,9 @@ export default function App () {
     const triviaCategories = useQuizCategories();
     const [score, setScore] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
+
+    const contentRef = useRef(null);
+    const [containerSize, setContainerSize] = useState({ width: 'auto', height: 'auto' });
 
     const checkSessionStatus = async () => {
         try {
@@ -33,45 +35,60 @@ export default function App () {
 
     useEffect(() => {
         if (status === 500) {
-            alert("Whoops, looks like there's a network error :/ \n Try refreshing in a moment")
-        } else {
-        if (triviaCategories && triviaQuestions.length > 0) {
+            alert("Whoops, looks like there's a network error :/ \n Try refreshing in a moment");
+        } else if (triviaCategories && triviaQuestions.length > 0) {
             setCurrentCategories(triviaCategories);
             setRemainingQuestions(triviaQuestions);
             setScore(0);
             setIsLoading(false);
         }
-    }
     }, [triviaCategories, triviaQuestions, status]);
+
+    // Effect to measure and set the size of the content container
+    useEffect(() => {
+        if (contentRef.current) {
+            const { width, height } = contentRef.current.getBoundingClientRect();
+            setContainerSize({ width, height });
+        }
+    }, [isLoggedIn, isLoading]); // Dependencies are isLoggedIn and isLoading
 
     const renderContent = () => {
         if (isLoading) {
-            return <div className={ Styles.spinner }></div>;
+            return <div className={Styles.spinner}></div>;
         } else if (isLoggedIn) {
             return <Quiz 
-                        setScore={ setScore }
-                        score={ score } 
-                        setRemainingQuestions={ setRemainingQuestions }
-                        setSelectedCategory={ setSelectedCategory } 
-                        remainingQuestions={ remainingQuestions } 
-                        fetchQuestions={ fetchQuestions }
-                        triviaQuestions={ triviaQuestions } 
-                        currentCategories={ currentCategories } 
-                        loggedIn={ setIsLoggedIn } 
+                        setScore={setScore}
+                        score={score}
+                        setRemainingQuestions={setRemainingQuestions}
+                        setSelectedCategory={setSelectedCategory}
+                        remainingQuestions={remainingQuestions}
+                        fetchQuestions={fetchQuestions}
+                        triviaQuestions={triviaQuestions}
+                        currentCategories={currentCategories}
+                        loggedIn={setIsLoggedIn}
                     />;
         } else {
-            return <Login loggedIn={ setIsLoggedIn } />;
+            return <Login loggedIn={setIsLoggedIn} />;
         }
     };
 
     return (
         <>
         <NavBar />
-        <div className={ Styles.mainSection }>
-            <div className={ Styles.dataSection }>
-                { renderContent() }
+        <div className={Styles.mainSection}>
+            <div 
+                className={Styles.dataSection} 
+                style={{ 
+                    width: containerSize.width, 
+                    height: containerSize.height,
+                    transition: 'width 0.3s ease-in-out, height 0.3s ease-in-out'
+                }}
+            >
+                <div ref={contentRef}>
+                    {renderContent()}
+                </div>
             </div>
         </div>
         </>
     );
-};
+}

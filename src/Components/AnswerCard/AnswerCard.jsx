345e774dc440cfe375c3810e6867692fa0950d53
“Matt” <mhallrp@gmail.com>
2023-12-29 19:53:47 +0100

    navbar

--- src/Components/AnswerCard/AnswerCard.jsx
+++ src/Components/AnswerCard/AnswerCard.jsx
@@ -2,7 +2,7 @@ import Styles from "./styles.module.css"
 
 export default function SkillCard (props) {
     return (
-        <div style={{ backgroundColor: props.correct ? "lightGreen" : props.selected ? "#effe16" : "#f5ff69"}}className={ Styles.answerCard } onClick={ props.onClick }>
+        <div style={{ backgroundColor: props.correct ? "lightOrange" : props.selected ? "#effe16" : "#f5ff69"}}className={ Styles.answerCard } onClick={ props.onClick }>
             <p>{ props.text }</p>
         </div>
     )

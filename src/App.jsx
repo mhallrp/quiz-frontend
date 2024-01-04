7138315acf489c792bea010950691a754bfa8d8a
“Matt” <mhallrp@gmail.com>
2024-01-04 16:59:32 +0100

    login UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,10 @@ export default function App() {
       }
   };
 
+  // useEffect(() => {
+  //     checkSessionStatus();
+  // }, []);
+
   useEffect(() => {
     setIsLoading(true)
       if (status === 500) {
@@ -49,19 +53,7 @@ export default function App() {
           setScore(0);
           checkSessionStatus();
       }
-  }, []);
-
-  // useEffect(() => {
-  //   setIsLoading(true)
-  //     if (status === 500) {
-  //         alert("Whoops, looks like there's a network error :/ \n Try refreshing in a moment");
-  //     } else if (triviaCategories && triviaQuestions.length > 0) {
-  //         setCurrentCategories(triviaCategories);
-  //         setRemainingQuestions(triviaQuestions);
-  //         setScore(0);
-  //         checkSessionStatus();
-  //     }
-  // }, [triviaCategories, triviaQuestions, status]);
+  }, [triviaCategories, triviaQuestions, status]);
 
   useEffect(() => {
       setContentOpacity(0);

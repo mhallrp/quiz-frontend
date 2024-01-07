7a4a619c47edab4bc4d48f94f3ed7045942c1d6f
“Matt” <mhallrp@gmail.com>
2024-01-07 12:12:46 +0100

    css to tailwind

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 const NavBar = (props) => {
   return (
-    <div className="flex z-10 align-middle relative w-full h-16 flex-row shadow-navbar bg-white">
-      <h1 className="text-greydark ml-2 mr-auto font-oswald text-xl leading-none sm:ml-20">
+    <div className="flex z-10 items-center relative w-full h-16 flex-row shadow-navbar bg-white">
+      <h1 className="text-greydark ml-2 mr-auto font-oswald text-2xl leading-none sm:ml-20">
         Quik Quiz 💡
       </h1>
       <h1
-        className="sm:mr:20 ml-auto mr-2 text-xl"
+        className="sm:mr-20 ml-auto mr-2 text-2xl"
         style={{
           opacity: props.opacity,
           transition: 'opacity 300ms ease-in-out',

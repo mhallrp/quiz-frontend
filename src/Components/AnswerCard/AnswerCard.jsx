e581055edaf27104b02fe93d73ad8d3472021a7c
“Matt” <mhallrp@gmail.com>
2024-01-07 16:05:12 +0100

    css to tailwind

--- src/Components/AnswerCard/AnswerCard.jsx
+++ src/Components/AnswerCard/AnswerCard.jsx
@@ -13,7 +13,7 @@ const SkillCard = (props) => {
 
   return (
     <div
-      className="border-greyanswerb text-center text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16"
+      className={`${backgroundClass} border-greyanswerb text-center text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}
       onClick={props.onClick}>
       <p>{props.text}</p>
     </div>

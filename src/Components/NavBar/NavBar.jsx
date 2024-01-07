c91d715f79a65a8224b475b0a4337e8e4bc8ce6f
“Matt” <mhallrp@gmail.com>
2024-01-07 13:30:41 +0100

    css to tailwind

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,13 +3,13 @@
 const NavBar = (props) => {
   return (
     <div className="flex items-center w-full min-h-16 flex-row shadow-navbar bg-white">
-      <div className="flex mx-2 sm:mx-20">
+      <div className="flex mx-2 sm:mx-20 w-full">
       <h1 className="text-greydark mr-auto font-oswald text-2xl leading-none">
         Quik Quiz 💡
       </h1>
       {/* Why isnt the below h1 aligned left in its parent div? */}
       <h1
-        className="ml-auto text-2xl w-full"
+        className="ml-auto text-2xl"
         style={{
           opacity: props.opacity,
           transition: 'opacity 300ms ease-in-out',

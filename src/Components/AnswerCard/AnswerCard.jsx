abe17cfbddc928542db6ba4185300adc71abe9bb
“Matt” <mhallrp@gmail.com>
2024-01-07 15:03:40 +0100

    css to tailwind

--- src/Components/AnswerCard/AnswerCard.jsx
+++ src/Components/AnswerCard/AnswerCard.jsx
@@ -10,9 +10,11 @@ export default function SkillCard(props) {
     }
   };
 
+  const backgroundClass = getBackgroundClass();
+
   return (
     <div
-className={`${getBackgroundClass} border-greyanswer text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}
+      className={`${backgroundClass} border-greyanswer text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}
       onClick={props.onClick}>
       <p>{props.text}</p>
     </div>

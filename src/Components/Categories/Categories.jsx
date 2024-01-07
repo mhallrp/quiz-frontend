085e7c8a2edb832822cc75abcbc0dfc3042ef357
“Matt” <mhallrp@gmail.com>
2024-01-07 17:54:06 +0100

    css to tailwind

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -2,7 +2,17 @@ const Categories = () => {
   return <div className="flex mx-2 mb-2.5 w-full rounded-xl bg-white sm:mx-20">
 
 <p className="text-2xl font-oswald">Quiz Topic</p>
-    
+{props.currentCategories && (
+        <select className="h-8" onChange={(e) => props.setSelectedCategory(e.target.value)}>
+          {props.currentCategories.map((e, index) => {
+            return (
+              <option key={index} value={e.id}>
+                {e.name}
+              </option>
+            );
+          })}
+        </select>
+      )}
 
   </div>;
 };

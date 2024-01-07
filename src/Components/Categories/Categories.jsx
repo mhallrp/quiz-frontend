25e72631e1436ddf455786f69debb5d39de19afd
“Matt” <mhallrp@gmail.com>
2024-01-07 15:40:03 +0100

    css to tailwind

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,8 +1,7 @@
 const Categories = () => {
   return (
-    <div className="rounded-25px border-l-greylight mx-2 flex w-auto flex-col items-center justify-center overflow-auto border-b bg-white p-6 sm:mx-20">
-CATEGORIES
-
+    <div className="rounded-25px border-l-greylight mx-2 flex w-full flex-col items-center justify-center overflow-auto border-b bg-white p-6 sm:mx-20">
+      CATEGORIES
     </div>
   );
 };

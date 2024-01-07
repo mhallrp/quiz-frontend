05ee58f9ebd2f85a30cb6c48de024bdcb116d2f6
“Matt” <mhallrp@gmail.com>
2024-01-07 17:55:58 +0100

    css to tailwind

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-const Categories = () => {
+const Categories = (props) => {
   return <div className="flex mx-2 mb-2.5 w-full rounded-xl bg-white sm:mx-20">
 
 <p className="text-2xl font-oswald">Quiz Topic</p>

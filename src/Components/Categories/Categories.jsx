6a1c3c85510c739eb6205ff557a8824a003a1e0c
“Matt” <mhallrp@gmail.com>
2024-01-07 15:27:08 +0100

    css to tailwind

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,7 +1,5 @@
 const Categories = () => {
-  return (
-    <div className="mx-2 my-6 h-16 w-full rounded-xl bg-white sm:mx-20"></div>
-  );
+  return <div className="mx-2 my-6 h-16 w-full rounded-xl bg-white"></div>;
 };
 
 export default Categories;

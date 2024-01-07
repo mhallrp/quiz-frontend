a2f4d03099fc5a445893969be531b0104adc1964
“Matt” <mhallrp@gmail.com>
2024-01-07 14:49:10 +0100

    css to tailwind

--- src/Components/AnswerCard/AnswerCard.jsx
+++ src/Components/AnswerCard/AnswerCard.jsx
@@ -7,7 +7,7 @@ export default function SkillCard(props) {
           : props.selected
             ? 'bg-selected'
             : 'bg-greyanswer'
-      } h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}
+      }`}
       onClick={props.onClick}>
       <p className="">{props.text}</p>
     </div>

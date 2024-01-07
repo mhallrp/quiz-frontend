6a0656e93e9f9df02cc41ccf765d4641b830b0c0
“Matt” <mhallrp@gmail.com>
2024-01-07 15:00:44 +0100

    css to tailwind

--- src/Components/AnswerCard/AnswerCard.jsx
+++ src/Components/AnswerCard/AnswerCard.jsx
@@ -1,24 +1,20 @@
 export default function SkillCard(props) {
+
+  const getBackgroundClass = () => {
+    if (props.correct) {
+      return 'bg-correct';
+    } else if (props.selected) {
+      return 'bg-selected';
+    } else {
+      return 'bg-greyanswer';
+    }
+  };
+
   return (
     <div
-      style={{
-        backgroundColor: props.correct
-          ? 'green'
-          : props.selected
-            ? '#effe16'
-            : '#f5ff69',
-      }}
-      className="border-greyanswer text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16"
+className={`${getBackgroundClass} border-greyanswer text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}
       onClick={props.onClick}>
       <p>{props.text}</p>
     </div>
   );
 }
-
-// className={`${
-//   props.correct
-//     ? 'bg-correct'
-//     : props.selected
-//       ? 'bg-selected'
-//       : 'bg-greyanswer'
-// } border-greyanswer text-greytext m-2 flex h-36 w-52 items-center justify-center rounded-xl border-2 p-16`}

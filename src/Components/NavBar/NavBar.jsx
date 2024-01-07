0ffe5a29cb9b4f2abedaca6b8c607a36b016ea9d
“Matt” <mhallrp@gmail.com>
2024-01-07 16:41:19 +0100

    css to tailwind

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,20 +1,19 @@
-
-
 const NavBar = (props) => {
   return (
-    <div className="flex items-center w-full min-h-16 flex-row bg-white border-b border-greylight">
-      <div className="flex mx-2 sm:mx-20 w-full">
-      <h1 className="text-greydark mr-auto font-oswald text-2xl leading-none">
-        Quik Quiz 💡
-      </h1>
-      <h1
-        className="ml-auto text-base text-greytext"
-        style={{
-          opacity: props.opacity,
-          transition: 'opacity 300ms ease-in-out',
-        }}>
-        {props.userData}
-      </h1>
+    <div className="border-greylight flex min-h-16 w-full flex-row items-center border-b bg-white">
+      <div className="mx-2 flex w-full sm:mx-20">
+        <h1 className="text-greydark mr-auto font-oswald text-2xl leading-none">
+          Quik Quiz 💡
+        </h1>
+        <h1
+          className="text-greytext ml-auto text-base"
+          style={{
+            opacity: props.opacity,
+            transition: 'opacity 300ms ease-in-out',
+          }}>
+          <div className="bg-darkYellow rounded text-black">{props.userData}</div>
+        </h1>
+        <button className="text-mustard underline">Logout</button>
       </div>
     </div>
   );

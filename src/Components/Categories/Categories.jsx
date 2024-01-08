d1a686e14864e55796c0dfef64027ab68769c0d0
“Matt” <mhallrp@gmail.com>
2024-01-09 00:06:25 +0100

    button updates

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,10 +1,10 @@
 const Categories = (props) => {
   return (
-    <div className=" mb-2.5 flex w-full items-center rounded-xl bg-white ">
-      <p className="m-6 font-oswald text-2xl">Quiz Topic</p>
+    <div className="flex mb-2.5  w-full items-center  rounded-xl bg-white ">
+      <p className="m-6 font-oswald text-2xl flex-shrink-0">Quiz Topic</p>
       {props.currentCategories && (
         <select
-          className="mr-6 mt-1 w-fit h-8 rounded border border-greylight bg-white pl-2 text-greytext"
+          className="mr-6 ml-auto mt-1 h-8 w-full rounded border border-greylight bg-white pl-2 text-greytext"
           onChange={(e) => props.setSelectedCategory(e.target.value)}>
           {props.currentCategories.map((e, index) => {
             return (
@@ -13,7 +13,6 @@ const Categories = (props) => {
               </option>
             );
           })}
-          
         </select>
       )}
     </div>
